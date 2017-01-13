cbc3b3119257e293e337a71cdcefdd543277e482
Mike Harder <mharder@microsoft.com>
2017-01-13 12:28:25 -0800

    Remove unnecessary `sudo`

--- install-docker.sh
+++ install-docker.sh
@@ -14,7 +14,7 @@ apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 . /etc/os-release
-echo "deb https://apt.dockerproject.org/repo $ID-$VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo $ID-$VERSION_CODENAME main" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
 
 apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
