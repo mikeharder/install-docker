520f45cfdd459c9a12804c0107d29b1a52069e3a
Mike Harder <mharder@microsoft.com>
2017-01-19 16:35:02 -0800

    Rename $ID to $DISTRIB_ID to match /etc/lsb-release

--- install-docker.sh
+++ install-docker.sh
@@ -14,7 +14,7 @@ apt-get install apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 . /etc/lsb-release
-echo "deb https://apt.dockerproject.org/repo $ID-$DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo $DISTRIB_ID-$DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
 
 apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
