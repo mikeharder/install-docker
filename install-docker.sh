3ef8fa3920cafac8415372c28dcca6c93528f25e
Mike Harder <mharder@microsoft.com>
2017-02-03 11:47:48 -0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -10,14 +10,24 @@ fi
 set -x
 
 apt-get update
-apt-get install apt-transport-https ca-certificates
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-get install curl \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
 
-. /etc/lsb-release
-echo "deb https://apt.dockerproject.org/repo $DISTRIB_ID-$DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/docker.list
+apt-get install apt-transport-https \
+                       ca-certificates
+
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+
+apt-get install software-properties-common
+add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
 apt-get update
-
-apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 apt-get -y install docker-engine
 
