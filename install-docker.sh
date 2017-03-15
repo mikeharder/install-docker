217ceba06744275478af7f3c03fbe0eb5f6917b6
Mike Harder <mharder@microsoft.com>
2017-03-15 12:40:43 -0700

    Update to use docker-ce

--- install-docker.sh
+++ install-docker.sh
@@ -9,29 +9,32 @@ fi
 # echo on
 set -x
 
+# remove older versions
+apt-get remove docker docker-engine
+
 apt-get update
-apt-get install curl \
+apt-get install \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual
 
-apt-get install apt-transport-https \
-                       ca-certificates
+apt-get install 
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
 
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
-
-apt-get install software-properties-common
 add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       ubuntu-$(lsb_release -cs) \
-       main"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 apt-get update
 
-apt-get -y install docker-engine
-
-service docker start
+apt-get -y install docker-ce
 
 groupadd docker
 usermod -aG docker $SUDO_USER
